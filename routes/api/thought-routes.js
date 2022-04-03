2a10a529d8d81bd1b2b700759bb0366ac935f100
kalen510 <kalen.wiley@gmail.com>
2022-04-02 23:08:29 -0400

    outline routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,29 @@
+const { Thought } = require('../../models');
+
+// /api/thoughts
+// GET all thoughts
+
+
+
+// GET a single thought by its _id
+
+
+
+// POST to create a new thought (also push the created thought's _id to the associated user's thoughts array field)
+
+
+
+// PUT to update a thought by its _id
+
+
+
+// DELETE to remove a thought by its _id
+
+
+
+// /api/thoughts/:thoughtId/reactions
+// POST to create a reaction stored in  a single thought's reactions array field
+
+
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
---
+++ routes/api/user-routes.js
@@ -0,0 +1,32 @@
+const { User } = require('../../models');
+
+
+// /api/users
+// GET all users 
+
+
+
+// GET a single user by _id
+
+
+
+// POST a new user 
+
+
+
+// PUT to update a user by its _id
+
+
+
+// DELETE to remove a user by its _id
+    // BONUS: remove a user's associated thoughts when deleted
+
+
+
+
+// /api/users/:userId/friends/:friendId
+// POST to add a new friend to a user's friend list
+
+
+
+// DELETE to remove a friend from a user's friend list
