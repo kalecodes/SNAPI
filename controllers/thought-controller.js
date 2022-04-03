20e3e80aa69b9865f99ead44482382dcfa53622d
kalen510 <kalen.wiley@gmail.com>
2022-04-03 13:18:05 -0400

    get all, get 1, create thought controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,11 +1,77 @@
-const { Thought } = require('../models/Thought');
+const { Thought, User } = require('../models');
 
-// getThoughts,
-// getSingleThought,
-// createThought,
-// updateThought,
-// deleteThought,
-// addReaction,
-// removeReaction
+const thoughtController = {
+    // get all thoughts 
+    getThoughts(req, res) {
+        Thought.find()
+            .sort({ createdAt: desc })
+            .then((dbThoughtData) => {
+                res.json(dbThoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    
+    //get single thought by id
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then((dbThoughtData) => {
+                if(!dbThoughtData) {
+                    return res.status(404).json({ message: 'No thought with this id!' });
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
 
+    // create a thought 
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((dbThoughtData) => {
+                return User.findOneAneUpdate(
+                    { _id: req.body.userId },
+                    { $push: { thoughts: dbThoughtData._id } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData)
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            })
+    },
 
+    // update a thought by id
+    updateThought(req, res) {
+
+    },
+
+    // delete a thought by id
+    deleteThought(req, res) {
+
+    },
+
+    // add reaction to a thought by id
+    addReaction(req, res) {
+
+    },
+
+    // delete a reaction by id
+    removeReaction(req, res) {
+
+    }
+
+}   
+
+module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,11 +1,40 @@
 const { User } = require('../models/User');
 
-// getUsers,
-// getSingleUser,
-// createUser,
-// updateUser,
-// deleteUser,
-// addFriend,
-// removeFriend
+const userController = {
+    // get all users
+    getUsers(req, res) {
 
+    },
 
+    // get a single user by _id
+    getSingleUser(req, res) {
+
+    },
+
+    // create a user 
+    createUser(req, res) {
+
+    },
+
+    // update a user by id
+    updateUser(req,res) {
+
+    },
+
+    // delete a user by id
+    deleteUser(req, res) {
+
+    },
+
+    // add a friend to a users friend list
+    addFriend(req, res) {
+
+    },
+
+    // remove a friend from a user's friend list
+    removeFriend(req, res) {
+
+    }
+}
+
+module.exports = userController;
