8ab4e6621feee5cb33483fcd175c5327a4031d2b
kalen510 <kalen.wiley@gmail.com>
2022-04-03 13:26:23 -0400

    find all, find 1, and create user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,19 +1,47 @@
-const { User } = require('../models/User');
+const { User, Thought } = require('../models');
 
 const userController = {
     // get all users
     getUsers(req, res) {
-
+        User.find()
+            .select('-__v')
+            .then((dbUserData) => {
+                res.json(dbUserData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
     },
 
     // get a single user by _id
     getSingleUser(req, res) {
-
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .populate('friends')
+            .populate('thoughts')
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    return res.status(404).json({ message: 'No user with this id!' });
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err)
+            });
     },
 
     // create a user 
     createUser(req, res) {
-
+        User.create(req.body)
+            .then((dbUserData) => {
+                res.json(dbUserData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
     },
 
     // update a user by id
